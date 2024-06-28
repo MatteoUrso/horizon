0b9523307304137befee0f9b20cb8e1d38f617fc
INFOR\matteou <matteourso100@gmail.com>
2024-06-28 10:34:29 +0200

    feat: Add new components for animated counter, header box, and total balance box

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,28 @@
+import HeaderBox from "@/components/header-box";
+import TotalBalanceBox from "@/components/total-balance-box";
 import React from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  const loggedIn = { firstName: "John" };
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions"
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
+  );
 }
